a18a34f2a38e95f7f92b1084691f1bcb5ab0a097
Duddie <marcin.dudar@gmail.com>
2024-06-18 17:26:19 +0800

    QuadSPI driver

---
+++ quadspi.h
@@ -0,0 +1,66 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern QSPI_HandleTypeDef hqspi;
+
+/* USER CODE BEGIN Private defines */
+uint8_t CSP_QUADSPI_Init(void);
+uint8_t CSP_QSPI_EraseSector(uint32_t EraseStartAddress);
+uint8_t CSP_QSPI_EraseSectors(uint32_t EraseStartAddress, uint32_t EraseEndAddress);
+
+uint8_t CSP_QSPI_EraseBlock(uint32_t flash_address);
+uint8_t CSP_QSPI_WriteMemory(uint8_t* buffer, uint32_t address, uint32_t buffer_size);
+uint8_t CSP_QSPI_EnableMemoryMappedMode(void);
+uint8_t CSP_QSPI_Erase_Chip (void);
+uint8_t QSPI_AutoPollingMemReady(void);
+uint8_t CSP_QSPI_Read(uint8_t* pData, uint32_t ReadAddr, uint32_t Size);
+
+/* USER CODE END Private defines */
+
+void MX_QUADSPI_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+/*W25Q32 memory parameters*/
+#define MEMORY_FLASH_SIZE				0x400000 /* 32Mbit =>4Mbyte */
+#define MEMORY_BLOCK_SIZE				0x10000   /*  blocks of 64KBytes */
+#define MEMORY_SECTOR_SIZE				0x1000    /* 4kBytes */
+#define MEMORY_PAGE_SIZE				0x100     /* 256 bytes */
+
+
+/*W25Q64JV commands */
+#define CHIP_ERASE_CMD 0xC7
+#define READ_STATUS_REG_CMD 0x05
+#define WRITE_ENABLE_CMD 0x06
+#define VOLATILE_SR_WRITE_ENABLE             0x50
+#define READ_STATUS_REG2_CMD 0x35
+#define WRITE_STATUS_REG2_CMD 0x31
+#define READ_STATUS_REG3_CMD 0x15
+#define WRITE_STATUS_REG3_CMD                0x11
+#define SECTOR_ERASE_CMD 0x20
+#define BLOCK_ERASE_CMD 0xD8
+#define QUAD_IN_FAST_PROG_CMD 0x32
+#define FAST_PROG_CMD 0x02
+#define QUAD_OUT_FAST_READ_CMD 0x6B
+#define DUMMY_CLOCK_CYCLES_READ_QUAD 8
+#define QUAD_IN_OUT_FAST_READ_CMD 0xEB
+#define RESET_ENABLE_CMD 0x66
+#define RESET_EXECUTE_CMD 0x99
+
+
+void MX_QUADSPI_Init(void);
+
+int32_t qspi_read_memory(uint32_t addr, void *data, uint32_t len);
+int32_t qspi_write_memory(uint32_t addr, void *data, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
